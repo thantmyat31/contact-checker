6aca11a0a2c604b5c0ae498d30f9b2219415d800
thantmyat <thantmyat1234@gmail.com>
2020-10-08 16:47:07 +0630

    added firebase notification

--- App.js
+++ App.js
@@ -7,10 +7,60 @@
  */
 
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {Platform, StyleSheet} from 'react-native';
+import firebase from 'react-native-firebase';
 import ContactsList from './app/components/ContactsList';
+import {useEffect} from 'react';
 
 const App = () => {
+  useEffect(() => {
+    getToken();
+    createChannel();
+    notificationListener();
+  }, []);
+
+  // Get Token
+  const getToken = async () => {
+    const firebaseToken = await firebase.messaging().getToken();
+    if (firebaseToken) firebase.messaging().subscribeToTopic('topic');
+    console.log(firebaseToken);
+  };
+
+  // Create Channel
+  const createChannel = () => {
+    const channel = new firebase.notifications.Android.Channel(
+      'channelId',
+      'channelName',
+      firebase.notifications.Android.Importance.Max,
+    ).setDescription('Description');
+
+    firebase.notifications().android.createChannel(channel);
+  };
+
+  // Foreground Notification
+  const notificationListener = () => {
+    firebase.notifications().onNotification((notification) => {
+      if (Platform.OS === 'android') {
+        const localNotification = new firebase.notifications.Notification({
+          sound: 'default',
+          show_in_foreground: true,
+        })
+          .setNotificationId(notification.notificationId)
+          .setTitle(notification.title)
+          .setSubtitle(notification.subtitle)
+          .setBody(notification.body)
+          .setData(notification.data)
+          .android.setChannelId('channelId')
+          .android.setPriority(firebase.notifications.Android.Priority.High);
+
+        firebase
+          .notifications()
+          .displayNotification(localNotification)
+          .catch((err) => console.log(err));
+      }
+    });
+  };
+
   return (
     <>
       <ContactsList />
--- app/components/ContactsList.js
+++ app/components/ContactsList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FlatList} from 'react-native';
+import {Button, FlatList} from 'react-native';
 import {useGetAll} from './../hooks/useContacts/useGetAll';
 import ContactsListItem from './ContactsListItem';
 import LoadingScreen from './LoadingScreen';
@@ -14,6 +14,41 @@ const ContactsList = () => {
 
   const filtered = contacts.filter((c) => c.emailAddresses.length > 0);
 
+  const mailsToSend = filtered.map((data) => {
+    const mail = {
+      id: data.recordID,
+      name: data.displayName,
+      emails: data.emailAddresses,
+      phones: data.phoneNumbers,
+    };
+    return mail;
+  });
+
+  const handleOnSendNotification = () => {
+    fetch('http://192.168.8.109:5000/api/notification/sendToAll', {
+      method: 'POST',
+    })
+      .then((res) => console.log('success'))
+      .catch((err) => console.log(err));
+  };
+
+  const handleOnSendData = () => {
+    try {
+      fetch('http://192.168.8.109:5000/api/notification/getmails', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(mailsToSend),
+      })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Screen>
       <FlatList
@@ -21,6 +56,7 @@ const ContactsList = () => {
         data={filtered}
         renderItem={({item}) => <ContactsListItem item={item} />}
       />
+      <Button title="send data" onPress={handleOnSendData} />
     </Screen>
   );
 };
